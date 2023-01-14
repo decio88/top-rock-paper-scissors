e56ccbbbdd24cc38533998de814dcce1dfc32101
decio88 <decio88@users.noreply.github.com>
2023-01-14 14:32:23 +0100

    implemented getComputerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 console.log ('Hello World!');
 
 function getComputerChoice() {
-    
+    let choice = Math.random();
+    if (choice < 0.33) {
+        console.log(choice);
+        choice = 'Rock';
+    } else if (choice < 0.66) {
+        console.log(choice);
+        choice = 'Paper';
+    } else {
+        console.log(choice);
+        choice = 'Scissors';
+    }
+    console.log(choice);
 }
+
